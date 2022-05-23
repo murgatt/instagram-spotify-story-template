cb29cc503934d2d7cb65663f4a321bfd70729771
murgatt <12082276+murgatt@users.noreply.github.com>
2022-05-23 18:36:13 +0200

    fix: auth config based on env variable

--- src/config/spotifyAuth.config.ts
+++ src/config/spotifyAuth.config.ts
@@ -1,5 +1,7 @@
+const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+
 const spotifyAuthEndpoint = 'https://accounts.spotify.com/authorize';
-const redirectUri = 'http://localhost:3000/signin'; // TODO replace with env variable
+const redirectUri = `${BASE_URL}signin`;
 const clientId = 'bf752e1cf26d43ca84b8514eef94d8a7';
 const loginUrl = `${spotifyAuthEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`;
 const sessionStorageAccessTokenKey = 'spotify-access-token';
