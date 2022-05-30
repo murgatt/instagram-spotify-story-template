bf4e6bffa4690381a69c899ab9d25bfc608a36f0
murgatt <12082276+murgatt@users.noreply.github.com>
2022-05-30 19:13:59 +0200

    feat: add width & height on all icons

--- src/components/icons/ArrowRightIcon.tsx
+++ src/components/icons/ArrowRightIcon.tsx
@@ -9,6 +9,8 @@ const ArrowRightIcon: Component<IconProps> = props => (
     viewBox="0 0 24 24"
     stroke="currentColor"
     stroke-width="2"
+    width={props.size}
+    height={props.size}
   >
     <path stroke-linecap="round" stroke-linejoin="round" d="M14 5l7 7m0 0l-7 7m7-7H3" />
   </svg>
--- src/components/icons/SearchIcon.tsx
+++ src/components/icons/SearchIcon.tsx
@@ -9,6 +9,8 @@ const SearchIcon: Component<IconProps> = props => (
     viewBox="0 0 24 24"
     stroke="currentColor"
     stroke-width="2"
+    width={props.size}
+    height={props.size}
   >
     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
   </svg>
